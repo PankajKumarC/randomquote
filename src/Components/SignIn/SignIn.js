3c50f693397aed93558d815b945b31692247ac7d
Mounick <40296148+Mounick@users.noreply.github.com>
2018-09-23 23:16:53 +0530

    updating onclick event

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 
-const SignIn = ({loggedIn}) => {
+const SignIn = ({onClickSignIn}) => {
 
 		return (
 			<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
@@ -24,7 +24,7 @@ const SignIn = ({loggedIn}) => {
 		      className="b ph3 pv2 input-reset green ba b--green bg-transparent hover-bg-white grow pointer f6 dib" 
 		      type="submit" 
 		      value="Sign in"
-		      onClick={ }/>
+		      onClick={() => onClickSignIn('success') }/>
 		    </div>
 		    <div className="lh-copy mt3">
 		      <p className="b ph3 pv2 input-reset green ba b--green bg-transparent hover-bg-white grow pointer f6 dib">Sign up</p>
